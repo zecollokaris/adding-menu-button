5e9a86eba3416fa46c82a6936bf869c6f2712523
zecollokaris <collo.kariss@gmail.com>
2018-02-25 22:50:52 +0300

    saving to git hb repo

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ //Opening the menu
+ $("#open-menu").click(function() {
+   $("aside").animate({"left": 0}, 2000);
+ });
+ //Closing the menu
+$("#close-menu").click(function() {
+ $("aside").animate({"left":"-100%"}, 2000);
+ });
+});
